a3fdecccf65bb4fa93443451354b98dc47778501
minimi61 <new1573@naver.com>
2022-06-22 21:31:01 +0900

    result commit

--- js/start.js
+++ js/start.js
@@ -2,69 +2,47 @@ const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 const result = document.querySelector('#result');
 const endPoint = 12;
-const select = [];
+const select = [0,0,0,0,0,0,0,0,0,0,0,0];
 
 const calResult = () => {
-    let pointArray = [
-        {
-            name : 'mouse', value : 0, key:0
-        },
-         {
-            name : 'cow', value : 0, key:1
-        },
-         {
-            name : 'tiger', value : 0, key:2
-        },
-         {
-            name : 'rabbit', value : 0, key:3
-        },
-         {
-            name : 'dragon', value : 0, key:4
-        },
-         {
-            name : 'snake', value : 0, key:5
-        },
-         {
-            name : 'horse', value : 0, key:6
-        },
-         {
-            name : 'sheep', value : 0, key:7
-        },
-         {
-            name : 'monkey', value : 0, key:8
-        },
-         {
-            name : 'chick', value : 0, key:9
-        },
-         {
-            name : 'dog', value : 0, key:10
-        },
-         {
-            name : 'pig', value : 0, key:11
-        }   
-    ]
-    for (let i = 0; i < endPoint; i++){
-        let target = qnaList[i].a[select[i]];
-        for (let j = 0; j < target.type.length; j++){
-            for (let k = 0; k < pointArray.length; k++){
-                if (target.type[j] === pointArray[k].name) {
-                    pointArray[k].value += 1;
-                }
-            }
-        }
-    }
-    let resultArray = pointArray.sort(function (a, b) {
-        if (a.value > b.value) {
-            return -1;
-        }
-        if (a.value < b.value) {
-            return 1
-        }
-        return 0;
-    })
-    console.log(resultArray)
-    let resultWord = resultArray[0].key;
-    return resultWord;
+    // let pointArray = [
+    //     {name : 'mouse', value : 0, key:0 },
+    //      {name : 'cow', value : 0, key:1 },
+    //      {name : 'tiger', value : 0, key:2 },
+    //      {name : 'rabbit', value : 0, key:3 },
+    //      {name : 'dragon', value : 0, key:4 },
+    //      {name : 'snake', value : 0, key:5 },
+    //      {name : 'horse', value : 0, key:6 },
+    //      {name : 'sheep', value : 0, key:7 },
+    //      {name : 'monkey', value : 0, key:8 },
+    //      {name : 'chick', value : 0, key:9 },
+    //      {name : 'dog', value : 0, key:10 },
+    //      {name : 'pig', value : 0, key:11 }   
+    // ]
+    // for (let i = 0; i < endPoint; i++){
+    //     let target = qnaList[i].a[select[i]];
+    //     for (let j = 0; j < target.type.length; j++){
+    //         for (let k = 0; k < pointArray.length; k++){
+    //             if (target.type[j] === pointArray[k].name) {
+    //                 pointArray[k].value += 1;
+    //             }
+    //         }
+    //     }
+    // }
+    // let resultArray = pointArray.sort(function (a, b) {
+    //     if (a.value > b.value) {
+    //         return -1;
+    //     }
+    //     if (a.value < b.value) {
+    //         return 1
+    //     }
+    //     return 0;
+    // })
+    
+    // console.log(resultArray)
+    // let resultWord = resultArray[0].key;
+    let result = select.indexOf(Math.max(...select));
+    return result;
 }
 
 
@@ -80,6 +58,7 @@ const goResult = () => {
         });
     }, 450);
     console.log(select)
+    setResult();
     calResult();
 }
 const answerButton = (addAnswer, qIdx, idx) => {
@@ -104,7 +83,11 @@ const answerButton = (addAnswer, qIdx, idx) => {
         }
 
         setTimeout(() => {
-            select[qIdx] = idx;
+            let target = qnaList[qIdx].a[idx].type;
+            for (let i = 0; i < target.length; i++) {
+                select[target[i]] += 1;
+            }
+            // select[qIdx] = idx;
             for (let i = 0; i < children.length; i++) {
                     children[i].style.display = 'none';
                 }
@@ -113,6 +96,23 @@ const answerButton = (addAnswer, qIdx, idx) => {
         //피연산자 앞에 위치한 전위 증가/감소 연산자는 먼저 피연산자의 값을 증가/감소시킨 후 다른 연산을 수행(++x)
 
     })
+}
+
+const setResult = () => {
+    let point = calResult();
+    const resultName = document.querySelector('.resultName');
+    resultName.innerHTML = infoList[point].name;
+
+    let resultImg = document.createElement('img');
+    const imgDiv = document.querySelector('#resultImg');
+    let imgURL = 'img/image-' + point + '.png';
+    resultImg.src = imgURL;
+    resultImg.alt = point;
+    resultImg.classList.add('img-fluid');
+    imgDiv.appendChild(resultImg);
+
+    const resultDesc = document.querySelector('.resultDesc');
+    resultDesc.innerHTML = infoList[point].desc;
 }
 
 const goNext = (qIdx) => {
@@ -145,65 +145,3 @@ const begin = () => {
         goNext(qIdx);
     },450)
 }
-
-
-
-
-
-
-
-// const main = document.querySelector("#main");
-// const qna = document.querySelector('#qna');
-
-// const addAnswer = (answerText,qIdx) => {
-//     let a = document.querySelector('.answerBox');
-//     let answer = document.createElement('button');
-//     answer.classList.add('answerList');
-//     answer.classList.add('my-3');
-//     answer.classList.add('py-3');
-//     answer.classList.add('mx-auto');
-//     answer.classList.add('fadeIn')
-
-//     a.appendChild(answer);
-//     answer.innerHTML = answerText;
-
-//     answer.addEventListener("click", function () {
-//         let children = document.querySelectorAll('.answerList');
-//         console.log(children)
-
-//         for (let i = 0; i < children.length; i++) {
-//             children[i].disabled = true;
-//             children[i].style.WebkitAnimation = "fadeOut 0.5s";
-//             children[i].style.animation = "fadeOut 0.5s";
-//         }
-//         setTimeout(() => {
-//             for (let i = 0; i < children.length; i++) {
-//                 children[i].style.display = 'none';
-//             }
-//             goNext(++qIdx);
-//         },450)
-//     }, false);
-// }
-
-// const goNext = (qIdx) => {
-//     let q = document.querySelector('.qBox');
-//     q.innerHTML = qnaList[qIdx].q;
-//     for (let i in qnaList[qIdx].a) {
-//         addAnswer(qnaList[qIdx].a[i].answer,qIdx);
-//     }
-// }
-// const begin = () => {
-//     main.style.WebkitAnimation = "fadeOut 1s";
-//     main.style.animation = "fadeOut 1s";
-//     setTimeout(() => {
-//         qna.style.WebkitAnimation = "fadeIn 1s";
-//         qna.style.animation = "fadeIn 1s";
-//         setTimeout(() => {
-//             main.style.display = "none";
-//             qna.style.display = "block";
-//         }, 450)
-//         let qIdx = 0;
-//         goNext(qIdx);
-//     }, 450);
-
-// }
